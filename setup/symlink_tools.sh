b1298fd35a1addfbec191b036979878e8cd65ae6
Sam Sheffres <ssheffres@gmail.com>
2015-12-04 12:55:57 +0600

    Make some tools more robust, add gp homr script

--- setup/symlink_tools.sh
+++ setup/symlink_tools.sh
@@ -3,6 +3,6 @@ while read TOOL
 do
     IFS=',' read -a tool <<< "$TOOL"
     echo "Symlinking ${tool[0]} as ${tool[1]}"
-    chmod +x ${tool[0]}
+    chmod +x /Users/samsheff/tools/${tool[0]}
     ln -sfv /Users/samsheff/tools/${tool[0]} /usr/local/bin/${tool[1]}
 done < $1
--- toolshed/new_tool.sh
+++ toolshed/new_tool.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "#!$(whereis $1)"  > "$2"
-vim $2
+echo "#!$(whereis $1)"  > "/Users/samsheff/tools/$2"
+vim "~/tools/$2"
