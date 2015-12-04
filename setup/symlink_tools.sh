83d64058579ff7504032ed5b2d94b8cadd8d091a
Sam Sheffres <ssheffres@gmail.com>
2015-12-04 09:35:50 +0600

    Add git shortcuts, move everything to subfolders

--- setup/symlink_tools.sh
+++ setup/symlink_tools.sh
@@ -3,5 +3,6 @@ while read TOOL
 do
     IFS=',' read -a tool <<< "$TOOL"
     echo "Symlinking ${tool[0]} as ${tool[1]}"
-    ./sym.sh ${tool[0]} ${tool[1]}
+    chmod +x ${tool[0]}
+    ln -sfv /Users/samsheff/tools/${tool[0]} /usr/local/bin/${tool[1]}
 done < $1
